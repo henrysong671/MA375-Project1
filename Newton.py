70d82f4e7ce0d8671e741996a68354378f6b21d4
Henry Song <henry.song671@gmail.com>
2021-03-25 21:34:53 +1000

    added error and random selection from interval

--- Newton.py
+++ Newton.py
@@ -4,31 +4,34 @@
 # Description: Method for Newton's method. Default iteration is set to 1000.
 #   - Inputs:(f, interval, x0)
 #   --- f: lambda representation of function
+#   === f_prime: lambda representation of derivitive of function
 #   --- interval: array contanining search interval (i.e. [-2, 0])
-#   --- x0: initial guess for root location
 #   - Output: approximation of root or None if DNE
 #==========================================================================
-
-def newton(f, f_prime, x0):
+import random
+def newton(f, f_prime, interval):
     # default iteration is set to 1000
     n = 1000
 
     # default error tolerance is set to 0.000001
     E = 0.000001
 
+    x0 = random.uniform(float(interval[0]), float(interval[1]))
+
+
     r = x0 - f(x0)/f_prime(x0)
 
     for i in range(n):
         # checks if root falls into error tolerance
-        if abs(r) >= E: 
+        if abs(r) >= E and abs(x0 >= E): 
             # checks if |f(x0)| < E
-            if abs(f(x0)) < E: return x0
+            if abs(f(x0)) < E: return x0, i
 
             xn = x0
             x0 = x0 - f(x0)/f_prime(x0)
 
             # checks if |xn-x| < E
-            if abs(xn - x0) < E: return x0
+            if abs(xn - x0) < E: return x0, i
         else:
             break
-    return r
+    return r, i
