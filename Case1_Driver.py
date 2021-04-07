a174ea8c2705e85ee6b67791becae52c199150b6
Henry Song <henry.song671@gmail.com>
2021-04-07 11:28:49 +1000

    finalized iteration comparison

--- Case1_Driver.py
+++ Case1_Driver.py
@@ -73,21 +73,29 @@ print()
 
 # sorts and prints methods from fastest execution time to slowest
 print("Fastest to slowest times\t\t\tFastest to slowest iterations")
+bool1 = bool2 = bool3 = bool4 = False
 sorted_times = sorted([execution_time1, execution_time2, execution_time3, execution_time4])
 sorted_iterations = sorted([bisection_result[1], newton_result[1], secant_result[1], falsi_result[1]])
-counter = 1
+#print(criteria)
 for i in range(len(sorted_times)):
-    print(counter, ":", end=" ")
+    print(i+1, ":", end=" ")
     if sorted_times[i] == execution_time1: print("Bisection Method", end="\t\t\t\t")
     elif sorted_times[i] == execution_time2: print("Newton's Method", end="\t\t\t\t")
     elif sorted_times[i] == execution_time3: print("Secant Method", end="\t\t\t\t")
     elif sorted_times[i] == execution_time4: print("Falsi Method", end="\t\t\t\t")
 
-    print(counter, ":", end=" ")
-    if sorted_iterations[i] == bisection_result[1]: print("Bisection Method")
-    elif sorted_iterations[i] == newton_result[1]: print("Newton's Method")
-    elif sorted_iterations[i] == secant_result[1]: print("Secant Method")
-    elif sorted_iterations[i] == falsi_result[1]: print("Falsi Method")
-    counter += 1
+    print(i+1, ":", end=" ")
+    if sorted_iterations[i] == bisection_result[1] and bool1 == False: 
+        print("Bisection Method")
+        bool1 = True
+    elif sorted_iterations[i] == newton_result[1] and bool2 == False: 
+        print("Newton's Method")
+        bool2 = True
+    elif sorted_iterations[i] == secant_result[1] and bool3 == False: 
+        print("Secant Method")
+        bool3 = True
+    elif sorted_iterations[i] == falsi_result[1] and bool4 == False: 
+        print("Falsi Method")
+        bool4 = True
 
 print()
